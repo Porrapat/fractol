c72f40522a82e53280172206dd52bd99995467b2
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-05-25 17:45:04 +0700

    Add More File

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -12,7 +12,12 @@
 
 #include "fractol.h"
 
-void	do_mandelbrot(void)
+// void	draw_mandelbrot(t_data *data)
+// {
+// 	(t_data *)data;
+// }
+
+void	do_mandelbrot_init(void)
 {
 	t_vars	vars;
 	t_data	img;
@@ -23,20 +28,73 @@ void	do_mandelbrot(void)
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 			&img.endian);
 	draw_grid_system(&img);
+	// draw_mandelbrot(&img);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	mlx_loop(vars.mlx);
 }
 
+void	do_julia_init(void)
+{
+	t_vars	vars;
+	t_data	img;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "Fractol");
+	img.img = mlx_new_image(vars.mlx, WIN_WIDTH, WIN_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	draw_grid_system(&img);
+	// draw_julia(&img);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_loop(vars.mlx);
+}
+
+void	do_burning_ship_init(void)
+{
+	t_vars	vars;
+	t_data	img;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "Fractol");
+	img.img = mlx_new_image(vars.mlx, WIN_WIDTH, WIN_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	draw_grid_system(&img);
+	// draw_julia(&img);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_loop(vars.mlx);
+}
+
+void	do_roberte_init(void)
+{
+	t_vars	vars;
+	t_data	img;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "Fractol");
+	img.img = mlx_new_image(vars.mlx, WIN_WIDTH, WIN_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	draw_grid_system(&img);
+	// draw_julia(&img);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_loop(vars.mlx);
+}
+
+
 void	check_fractal(char **argv)
 {
 	if (!ft_strncmp(argv[1], "mandelbrot", ft_strlen("mandelbrot")))
-		do_mandelbrot();
+		do_mandelbrot_init();
 	else if (!ft_strncmp(argv[1], "julia", ft_strlen("julia")))
-		printf("julia\n");
+		do_julia_init();
+		// printf("julia\n");
 	else if (!ft_strncmp(argv[1], "burning-ship", ft_strlen("burning-ship")))
-		printf("burning-ship\n");
+		do_burning_ship_init();
+		// printf("burning-ship\n");
 	else if (!ft_strncmp(argv[1], "roberte", ft_strlen("roberte")))
-		printf("roberte\n");
+		do_roberte_init();
+		// printf("roberte\n");
 }
 
 int	main(int argc, char **argv)
@@ -48,11 +106,7 @@ int	main(int argc, char **argv)
 	return (0);
 }
 /*
+	// do mandelbrot
+	// do julia
 	// do complex abs
-	// do grid system
-	// ...
-
-	// vars.mlx = mlx_init();
-	// vars.win = mlx_new_window(vars.mlx, WIN_WIDTH, WIN_HEIGHT, "Fractol");
-	// mlx_loop(vars.mlx);
 */
